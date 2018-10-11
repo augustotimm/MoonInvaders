8c9c5365f3b72054e7ae139af88744b5b72cdc72
augustotimm <augusto.tes@gmail.com>
2018-10-11 19:03:26 -0300

    Criada funcao de mover tela para a esquerda

--- Funcional/src/main.rs
+++ Funcional/src/main.rs
@@ -2,7 +2,9 @@
 
 
 fn main() {
-
+    let mut screen_arr: Vec<String> = vec![String::new(); 126];
+    screen_arr[0] = String::from(initialize_screen());
+    println!("Screen ARR{}",screen_arr[0]);
     let screen = initialize_screen();
     println!("{:?}",String::from(screen));
 
@@ -12,9 +14,16 @@ fn main() {
 
     let mut indiv_char = chars.next();
 
-    let mut result = move_right(indiv_char, chars);
+    let mut result = move_right(indiv_char.clone(), chars.clone());
 
     println!("{}",result);
+
+    let mut chars_r = result.chars();
+
+    let mut indiv_char_r = chars_r.next();
+    let mut result2 = move_left(indiv_char_r.clone(), chars_r.clone());
+
+    println!("{}",result2);
 
 }
 
@@ -28,7 +37,7 @@ fn move_right(indiv: Option<char>,mut  _line:std::str::Chars<'_>)-> String{
         Some(x)=>{
             buf.push('-');
             buf.push(x);
-            return move_rest(_line.next(), _line, buf.clone());
+            return move_rest_right(_line.next().clone(), _line.clone(), buf.clone());
             
         } ,
         None=> buf = String::new(),
@@ -36,12 +45,12 @@ fn move_right(indiv: Option<char>,mut  _line:std::str::Chars<'_>)-> String{
     buf
 }
 
-fn move_rest(indiv: Option<char>, mut original_line:std::str::Chars<'_>, mut new_line:String)-> String{
+fn move_rest_right(indiv: Option<char>, mut original_line:std::str::Chars<'_>, mut new_line:String)-> String{
     match indiv{
         Some(x)=>{
             new_line.push(x);
-            let mut rest = move_rest(original_line.next(), original_line, new_line.clone());
-            println!("{}", rest);
+            let mut rest = move_rest_right(original_line.next().clone(), original_line.clone(), new_line.clone());
+            
             return rest;
             //new_line.push_str(&rest);
             //println!("{}",new_line);
@@ -50,6 +59,39 @@ fn move_rest(indiv: Option<char>, mut original_line:std::str::Chars<'_>, mut new
         },
         None=>{
             new_line.pop();
+            return new_line;
+        },
+    }
+
+
+    
+}
+
+fn move_left(indiv: Option<char>,mut  _line:std::str::Chars<'_>)-> String{
+    let mut buf = String::with_capacity(5);
+    match indiv{
+        Some(x)=>{
+            return move_rest_left(_line.next().clone(), _line.clone(), buf.clone());
+            
+        } ,
+        None=> return String::new(),
+    }
+}
+
+fn move_rest_left(indiv: Option<char>, mut original_line:std::str::Chars<'_>, mut new_line:String)-> String{
+    match indiv{
+        Some(x)=>{
+            new_line.push(x);
+            let mut rest = move_rest_left(original_line.next().clone(), original_line.clone(), new_line.clone());
+            
+            return rest;
+            //new_line.push_str(&rest);
+            //println!("{}",new_line);
+            //return new_line;
+            
+        },
+        None=>{
+            new_line.push('-');
             return new_line;
         },
     }
