9dacf71e8737e09f3e55e693b57524ad050d116a
augustotimm <augusto.tes@gmail.com>
2018-10-15 01:58:41 -0300

    criada funcao para transposicao de matriz, para assim fazer movimentos verticais na tela

--- Funcional/src/main.rs
+++ Funcional/src/main.rs
@@ -2,16 +2,28 @@
 
 
 fn main() {
-    let mut screen_arr: Vec<String> = vec![String::new(); 126];
-    screen_arr[0] = String::from(initialize_screen());
+    
+    let mut screen_arr: Vec<String> = Vec::new();
+    screen_arr.push(String::from(initialize_screen()));
+    screen_arr.push(String::from(initialize_screen()));
+    transpose_string_vec(screen_arr.clone());
     println!("Screen ARR{}",screen_arr[0]);
     let screen = initialize_screen();
-    println!("{:?}",String::from(screen));
+    println!("{}",String::from(screen));
+    
 
     let count = screen.chars().count();
     
     let mut chars = screen.chars();
+    
 
+
+    let mut charr:Vec<char> = screen.chars().collect();
+    //let s: String = charr.into_iter().collect();
+    //println!("{}",s);
+    
+
+/*
     let mut indiv_char = chars.next();
 
     let mut result = move_right(indiv_char.clone(), chars.clone());
@@ -24,8 +36,12 @@ fn main() {
     let mut result2 = move_left(indiv_char_r.clone(), chars_r.clone());
 
     println!("{}",result2);
+  */  
+
 
 }
+
+
 
 fn initialize_screen()-> &'static str{
 
@@ -99,3 +115,68 @@ fn move_rest_left(indiv: Option<char>, mut original_line:std::str::Chars<'_>, mu
 
     
 }
+
+fn transpose_string_vec(s_vec:Vec<String>){
+    let mut s_char = stringvec_to_charvec(s_vec.clone());
+    println!("{:?}",s_char);
+    let mut result = transpose_vec(s_char.clone());
+    println!("{:?}",result);
+
+
+}
+
+
+fn stringvec_to_charvec(mut s:Vec<String>)-> Vec<Vec<char>>{
+    if s.len()<1{
+        return Vec::new();
+    }
+    else{
+        let mut buf:Vec<Vec<char>> = Vec::new();
+        buf.push(s[0].chars().collect());
+        s.remove(0);
+        let mut rest =stringvec_to_charvec(s.clone());
+        buf.append(&mut rest);
+            
+        return buf;
+    }
+}
+
+fn transpose_vec(v:Vec<Vec<char>>)-> Vec<Vec<char>>{
+    let mut buf:Vec<Vec<char>> = Vec::new();
+    
+    buf = init_transpose(v.clone(),buf, 0);
+
+    fn transpose(v:Vec<Vec<char>>,mut buffer:Vec<Vec<char>>, c:usize, l:usize)-> Vec<Vec<char>>{
+        
+        if l>= v.len() {
+            return buffer;
+        }
+        else{
+            if c>=v[l].len(){
+                return transpose(v, buffer, 0, l+1);
+            }
+            else{
+                buffer[c][l] = v[l][c];
+                
+                
+                return transpose(v.clone(), buffer.clone(), c+1, l);
+            }
+        }
+
+    }
+    fn init_transpose(v:Vec<Vec<char>>,mut buf:Vec<Vec<char>>, iter:usize)->Vec<Vec<char>>{
+        if iter>= v[0].len(){
+            return buf;
+        }
+        else{
+            
+            buf.push(vec!['0';v.len()]);
+            return init_transpose(v.clone(), buf.clone(), iter+1);
+        }
+    }
+
+    buf = transpose(v.clone(), buf.clone(), 0, 0);
+    return buf;
+}
+
+
